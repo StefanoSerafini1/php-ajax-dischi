d2a5feebb27ec25cef37bb59c9720f61352ec86e
StefanoSerafini <stefanoserafini7@gmail.com>
2020-12-17 22:21:31 +0100

    scss end

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="dist/css/main.css">
     <title>PHP-ajax-dischi</title>
 </head>
 <body>
@@ -11,20 +14,20 @@
     ?>
     <div id="app">
         <header class="header">
-            <div class="container">
-                <img src="" alt="Spotify">
+            <div class="logo">
+                <img src="dist/img/logo.png" alt="Spotify">
             </div>
         </header>
         <div class="container">
+        <?php foreach ($database as $song) {?>
             <div class="card">
-                <?php foreach ($database as $song) {?>
                     <img src="<?php echo $song['poster'] ?>" alt="">
                     <h3><?php echo $song['title'] ?></h3>
                     <h4><?php echo $song['author'] ?></h4>
                     <h4><?php echo $song['year'] ?></h4>
                     <span><?php echo $song['genre'] ?></span>
-                <?php } ?>
             </div>
+            <?php } ?>
         </div>
     </div>
 </body>
